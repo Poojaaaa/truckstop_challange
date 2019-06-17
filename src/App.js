5f54ac71efd42fa0412cc068036dc05fcfeefc7f
Poojaaaa <Pooja.gajera11@gmail.com>
2019-06-16 21:52:54 -0700

    Changed readme

--- src/App.js
+++ src/App.js
@@ -3,154 +3,88 @@ import charactersFile from "./data/characters.json"
 import axios from 'axios';
 import './App.css';
 
+const Error = ({ message }) => <h3>{message}</h3>;
+
+const Loader = () => (
+  <div style={{ marginTop: "20px", padding: "20px" }}>"Loading..."</div>
+);
+
+const List = ({ handleClick }) => (
+  <ul>
+    {charactersFile.characters.map(character => {
+      return (
+        <li key={character.name} onClick={() => handleClick(character)}>
+          {character.name}
+        </li>
+      );
+    })}
+  </ul>
+);
+
+const Content = ({ movies }) => (
+  <ul>
+    {movies.map(movie => (
+      <li key={movie.title}>
+        {movie.title} - {movie.release_date}
+      </li>
+    ))}
+  </ul>
+);
+
 class App extends Component {
 
   state = {
     movies: [],
-    loading: true,
+    loading: false,
     error: ''
   };
-  componentDidMount() {
-
-  }
 
   handleClick = character => {
+    // Set loading to true and error to false
+    this.setState({ loading: true, error: false, movies: [] });
     console.log(character.name);
-    const PeopleUrl = `https://swapi.co/api/people/`;
-    const FilmUrl = `https://swapi.co/api/films/`;
 
-    switch (character.name) {
-      case "Luke Skywalker":
-        axios
-          .get(`${PeopleUrl}1/`)
-          .then(response =>
-            Promise.all([
-              axios.get(`${FilmUrl}2/`),
-              axios.get(`${FilmUrl}6/`),
-              axios.get(`${FilmUrl}3/`),
-              axios.get(`${FilmUrl}1/`),
-              axios.get(`${FilmUrl}7/`)
-            ])
-          )
-          .then(result =>
-            result.map(values =>
-              this.setState({
-                movies: [
-                  ...this.state.movies,
-                  {
-                    title: values.data.title,
-                    release_date: values.data.release_date
-                  }
-                ],
-                loading: false,
-                render: true
-              })
-            )
-          );
-        break;
-
-      case "C-3PO":
-        axios
-          .get(`${PeopleUrl}2/`)
-          .then(response =>
-            Promise.all([
-              axios.get(`${FilmUrl}2/`),
-              axios.get(`${FilmUrl}5/`),
-              axios.get(`${FilmUrl}4/`),
-              axios.get(`${FilmUrl}6/`),
-              axios.get(`${FilmUrl}3/`),
-              axios.get(`${FilmUrl}1/`)
-            ])
-          )
-          .then(result =>
-            result.map(values =>
-              this.setState({
-                movies: [
-                  ...this.state.movies,
-                  {
-                    title: values.data.title,
-                    release_date: values.data.release_date
-                  }
-                ],
-                loading: false,
-                render: true
-              })
-            )
-          );
-        break;
-
-      case "Leia Organa":
-        axios.get(`${PeopleUrl}unknown/`)
-          .then(response => console.log('response.data')
-          ).catch(error => { throw (error) })
-        break;
-
-      case "R2-D2":
-        axios
-          .get(`${PeopleUrl}3/`)
-          .then(response =>
-            Promise.all([
-              axios.get(`${FilmUrl}2/`),
-              axios.get(`${FilmUrl}5/`),
-              axios.get(`${FilmUrl}4/`),
-              axios.get(`${FilmUrl}6/`),
-              axios.get(`${FilmUrl}3/`),
-              axios.get(`${FilmUrl}1/`),
-              axios.get(`${FilmUrl}7/`)
-            ])
-          )
-          .then(result =>
-            result.map(values =>
-              this.setState({
-                movies: [
-                  ...this.state.movies,
-                  {
-                    title: values.data.title,
-                    release_date: values.data.release_date
-                  }
-                ],
-                loading: false,
-                render: true
-              })
-            )
-          );
-        break;
-
-      default:
-        return "No list item";
-    }
+    axios
+      .get(character.url)
+      .then(({ data }) =>
+        Promise.all(data.films.map(filmUrl => axios.get(filmUrl)))
+      )
+      .then(result => {
+        const movies = result.map(({ data: { title, release_date } }) => ({
+          title,
+          release_date
+        }));
+        this.setState({ movies, loading: false, error: "" });
+      })
+      .catch(() => {
+        this.setState({
+          movies: [],
+          loading: false,
+          error: "List not found"
+        });
+      });
   };
 
   render() {
-    console.log(this.state);
-    const Content = this.state.loading ? (
-      <div style={{ marginTop: "20px", padding: "20px" }}>"Loading..."</div>
-    ) : (
-        <ul>
-          {this.state.movies.map(movie => (
-            <li key={movie.title}>
-              {movie.title} - {movie.release_date}
-            </li>
-          ))}
-        </ul>
-      );
+    const { error, loading, movies } = this.state;
 
-    const List = (
-      <ul>
-        {charactersFile.characters.map(character => {
-          return (
-            <li
-              key={character.name}
-              onClick={() => this.handleClick(character)}
-            >
-              {character.name}
-            </li>
-          );
-        })}
-      </ul>
-    );
+    return (
+      <div className="App">
+        {/* include Content component only when there is data */}
+        {movies.length > 0 ? (
+          <Content movies={movies} />
+        ) : (
+            <List handleClick={this.handleClick} />
+          )
+        }
 
-    return <div className="App">{!this.state.render ? List : Content}</div>;
+        {/* include Loader component when loading */}
+        {loading && <Loader />}
+
+        {/* include Error component when there is an error */}
+        {error && <Error message={error} />}
+
+      </div>);
   }
 }
 
